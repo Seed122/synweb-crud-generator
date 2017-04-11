985fc193f28eb60a0cde744f9aee85835d5aca30
Seed <srgsemenoff@gmail.com>
2017-04-11 22:35:48 +0300

    Extracted column name strings, added support for autogenerated guids

---
+++ SPGenerator.DAL/Meta/SysSchemasCols.cs
@@ -0,0 +1,12 @@
+﻿namespace SPGenerator.DAL.Meta
+{
+    /// <summary>
+    /// Columns that are in table sys.schemas
+    /// </summary>
+    public static class SysSchemasCols
+    {
+        public const string NAME = "name";
+        public const string SCHEMA_ID = "schema_id";
+        public const string PRINCIPAL_ID = "principal_id";
+    }
+}
